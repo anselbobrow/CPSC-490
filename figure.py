576438ae56b67c8d10ed08fcd6b6129f9ba58f16
Ansel Bobrow <ansel.bobrow@gmail.com>
2023-12-17 15:38:51 -0500

    reformat

--- figure.py
+++ figure.py
@@ -1,5 +1,5 @@
-from scipy.io import wavfile
-import level_crossing as lc
+# from scipy.io import wavfile
+# import level_crossing as lc
 import matplotlib.pyplot as plt
 import numpy as np
 #
@@ -14,8 +14,14 @@ import numpy as np
 # print(err)
 #
 x_values = np.arange(4)
-y1_values = [0.1391718088305171, 0.1340380337313888, 0.1341109435465007, 0.1340997842032969]
-y2_values = [0.23281247649448122, 0.24303587098079826, 0.1341109435465007, 0.02273627542004199]
+y1_values = [
+    0.1391718088305171, 0.1340380337313888, 0.1341109435465007,
+    0.1340997842032969
+]
+y2_values = [
+    0.23281247649448122, 0.24303587098079826, 0.1341109435465007,
+    0.02273627542004199
+]
 
 # Plot the first array (sin(x))
 plt.plot(x_values, y1_values, label='Bit-depth', color='blue', linestyle='-')
